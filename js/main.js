732982b8be3f6e941eab469765d3c8f3101b03d9
inovaforms <81268475+inovaforms@users.noreply.github.com>
2021-03-24 15:19:48 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-$(document).ready(function(){
-  // TABS
-  $('ul.tabs').tabs();
-});
-
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+  var elem = document.querySelectorAll('select');
+  var options = document.querySelectorAll("options");
+  var instances = M.FormSelect.init(elem, options);
   });
 
   // Or with jQuery
@@ -14,32 +10,29 @@ document.addEventListener('DOMContentLoaded', function() {
     $('select').formSelect();
   });
 
-  //Modal
+$(document).ready(function(){
+  // TABS
+  $('ul.tabs').tabs();
+});
 
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.modal').modal();
+//Modal
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
 });
 
 // botoes repostas e resultados
 let buttonClick1 = document.getElementById('meubotao1'); //O botao criado deve ter o campo id="meubotao"
 let buttonClick2 = document.getElementById('meubotao2');
 
+console.log(buttonClick1);
 buttonClick1.addEventListener("click",function(event){
     window.location.href = "../src formulario visualizar/forms_respostas_pelvica.html";
     console.log("Tentei redirecionar!!!");
 });
-
+console.log(buttonClick2);
 buttonClick2.addEventListener("click",function(event){
-
-//Usando esse método você vai para outra página e não pode usar o voltar do navegador    
-window.location.href = "../src formulario concluido/forms_respostas.html";
+    window.location.href = "../src formulario concluido/forms_respostas.html";
     console.log("Tentei redirecionar!!!");
 });
 
